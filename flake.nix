18719c0e15e9c37f921adb5553b828b328efd62e
Alberto G. Jácome <contact@agjacome.dev>
2024-12-07 22:20:15 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git
+            pkgs.tree-sitter
+
+            pkgs.unison-ucm
+          ];
+        };
+      }
+    );
+}
